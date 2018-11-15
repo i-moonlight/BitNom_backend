eb9d03b0240c460320837745e9ffe0aa0bea87e5
cryarchy <crystalflow.mwangi@gmail.com>
2018-11-15 16:11:01 +0300

    Enables CORS.

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ mongoose.connect(
 	}
 );
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(
 	"/graphql",
 	graphqlHTTP({
