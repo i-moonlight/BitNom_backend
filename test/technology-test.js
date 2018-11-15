fbc1a98db206cabd049a4d21f3abd3eb3d224683
i-moonlight <infinite.moonlight0506@gmail.com>
2018-11-15 16:55:48 +0300

    Added test templates.

---
+++ test/technology-test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+describe("technology", () => {
+	describe("query", () => {
+		describe("get", () => {});
+		describe("search", () => {});
+	});
+
+	describe("mutation", () => {
+		describe("create", () => {});
+		describe("vote", () => {});
+		describe("delete", () => {});
+		describe("update", () => {});
+	});
+});
---
+++ test/user-test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe("user", () => {
+	describe("query", () => {
+		describe("get", () => {});
+		describe("search", () => {});
+	});
+
+	describe("mutation", () => {
+		describe("create", () => {});
+		describe("changePassword", () => {});
+		describe("updateDisplayName", () => {});
+		describe("updateAccessGroup", () => {});
+		describe("resetPassword", () => {});
+		describe("delete", () => {});
+	});
+});
