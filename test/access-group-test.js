a1f3454749782f8d34ada7b4c21d34b660122c95
i-moonlight <infinite.moonlight0506@gmail.com>
2018-11-26 02:20:24 +0300

    Test: accessGroup-get > should retrieve by provided name.

--- test/access-group-test.js
+++ test/access-group-test.js
@@ -159,7 +159,64 @@ describe("accessGroup", () => {
 					.catch(helpers.logError(done));
 			});
 
-			it("should retrieve by provided name");
+			it("should retrieve by provided name", done => {
+				const accessGroup = {
+					name: "canGetAccessGroup",
+					permissions: [
+						{
+							model: "accessGroup",
+							endpoint: "get"
+						}
+					]
+				};
+				const user = {
+					email: "example@email.com",
+					access: mongoose.Types.ObjectId(),
+					date: new Date(),
+					password: "password",
+					verificationString: "verificationString"
+				};
+				mongoose
+					.model("AccessGroup")
+					.create(accessGroup)
+					.then(({ _id }) => {
+						return mongoose
+							.model("User")
+							.create(Object.assign(user, { access: _id }));
+					})
+					.then(user => {
+						return helpers.login(
+							"example@email.com",
+							"password",
+							done
+						);
+					})
+					.then(token => {
+						const variables = {
+							name: "canGetAccessGroup",
+							pagination: {}
+						};
+						return helpers.runQuery(
+							{ query, variables },
+							token,
+							done
+						);
+					})
+					.then(response => {
+						expect(response).not.to.be.undefined;
+						expect(response.body.data).not.to.be.undefined;
+						expect(response.body.data.accessGroup).not.to.be
+							.undefined;
+						expect(response.body.data.accessGroup.get).not.to.be
+							.null;
+						expect(
+							response.body.data.accessGroup.get.length
+						).to.equal(1);
+						done();
+					})
+					.catch(helpers.logError(done));
+			});
+
 			it("should retrive all is _id and/or name is not provided");
 		});
 	});
