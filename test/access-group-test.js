5255e7998461675ee9c2b7520bcafb3367859d24
i-moonlight <infinite.moonlight0506@gmail.com>
2018-11-27 15:40:36 +0300

    Test: coin-get > should retrieve by provided _id.

--- test/access-group-test.js
+++ test/access-group-test.js
@@ -5,7 +5,7 @@ const { expect } = require("chai");
 
 const helpers = require("./test-helpers");
 
-describe("accessGroup", () => {
+xdescribe("accessGroup", () => {
 	describe("query", () => {
 		beforeEach(done => {
 			mongoose
