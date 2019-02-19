0c54bea6e908bd2e9c2f2ecbe6b053f0ded2d749
i-moonlight <infinite.moonlight0506@gmail.com>
2019-02-19 17:14:28 +0300

    Added message APIs. Added support for SPA routing.

--- models/message.js
+++ models/message.js
@@ -5,12 +5,13 @@ const mongoose = require("mongoose");
 // define the schema
 const schema = mongoose.Schema({
 	// fields
-	avatar: { type: String, required: true },
-	user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-	recepient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-	thread: { type: mongoose.Schema.Types.ObjectId },
-	upvotes: { type: Number, min: 0, default: 0, required: true },
-	flags: { type: Number, min: 0, default: 0, required: true },
+	sender: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true
+	},
+	recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+	text: { type: String, required: true },
 	date: { type: Date, required: true },
 	read: { type: Boolean, required: true, default: false }
 });
